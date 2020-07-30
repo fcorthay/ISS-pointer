412f8d2f9c5cf12e0f617bb72fee96f6242ddcc0
RPi controller <raspberry.pi@hevs.ch>
2020-07-30 15:56:08 +0100

    Alex's new vesion

--- motors_control/Tests/test_I2C.py
+++ motors_control/Tests/test_I2C.py
@@ -3,11 +3,14 @@ import rpI2C
 
 INDENT = '  '
 
-address = 0x60
-bus = rpI2C.I2C(address)
+chip_address = 0x60
+register_address = 0x03
+bus = rpI2C.I2C(chip_address)
 
-print("reading data at address {%h}".format(address))
-data = bus.read_raw_byte()
-print(INDENT + "found {%h}".format(data))
+print("reading data from chip {:02X}, register {:02X}"
+  .format(chip_address, register_address))
+#data = bus.read_raw_byte()
+data = bus.read_unsigned_byte(register_address)
+print(INDENT + "found {:02X}".format(data))
 #bus.write_raw_byte(0x00)
 bus.clean_up()
